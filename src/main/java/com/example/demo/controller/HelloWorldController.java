0531ab13899fb863274973b8d5bbdf4a17644dfa
komura0053 <140610631+komura0053@users.noreply.github.com>
2023-07-30 11:56:37 +0900

    Initial Commit2

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-public class Controller {
+@Controller
+public class HelloWorldController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
+        model.addAttribute("message", "Hello World!!");
         return "index";
     }
 }
